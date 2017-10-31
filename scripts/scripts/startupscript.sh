c6d41508f62f0843759ac724d1e2477299957298
Andre <mctech26@gmail.com>
2017-10-31 14:40:04 +0100

    Added modifications due to i3lock

--- scripts/scripts/startupscript.sh
+++ scripts/scripts/startupscript.sh
@@ -2,7 +2,10 @@
 wal -R 
 walcolors.sh
 mpdscribble
+killall polybar
 polybar example & disown
 mpd 
 xrandr -s 1600x900
+compton & disown
+i3lock -i "bgs/702seconds.png" -e -f
 notify-send "Startup Script Completed!" "Have fun."
