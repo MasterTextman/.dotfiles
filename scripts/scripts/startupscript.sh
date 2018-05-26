dea6fb0c7799344bacfbbcedc573815afa73f7b7
Andre' Jenkins <mctech26@gmail.com>
2018-05-26 23:17:47 +0200

    Updated scripts, polybar, and added alias

--- scripts/scripts/startupscript.sh
+++ scripts/scripts/startupscript.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-wal -R 
-walcolors.sh
-mpdscribble
-killall polybar
+wal -R                                           # Loads previously loaded bg
+walcolors.sh                                     # Loads the colors by wal
+mpdscribble                                      # Loads scribbler daemon
+systemctl --user start mpdscribble.service       # Starts scribbler service
+systemctl --user enable mpdscribble.service      # Enables scribbler
+sudo battery-stats-collector & disown            # Starts battery stats
+killall polybar                                  # Used to reset polybar
 polybar example & disown
-mpd 
-#xrandr -s 1600x900
-compton & disown
-i3lock -i "bgs/702seconds.png" -e -f
-nm-applet
-notify-send "Startup Script Completed!" "Have fun."
+mpd                                              # Starts music player daemon
+#xrandr -s 1600x900                             
+compton & disown                                 # Starts compositor
+i3lock -i "bgs/702seconds.png" -e -f             # Starts screen-locker
